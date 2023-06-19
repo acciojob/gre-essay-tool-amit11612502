5a36a1f2d6acae0b1e43415883a455f56e1818d6
amit11612502 <38473703+amit11612502@users.noreply.github.com>
2023-06-19 17:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your code here
-function countWord() {
-      var words = document.getElementById("evaluatedText").value;
-            var count = 0;
-            var split = words.split(' ');
-            for (var i = 0; i < split.length; i++) {
-                if (split[i] != "") {
-                    count += 1;
-                }
-            }
-	 document.getElementById("wordCount").innerText = count;
+function fun() {
+var str = document
+        .getElementById("evaluatedText").value;
+var count = 0;
+var splited = str.split(' ');
+for (var i = 0; i < splited.length; i++) {
+        if (splited[i] != "") {
+            count += 1;
+        }
+		}
+	document.getElementById("wordCount")
+        .innerHTML = count;
 }
